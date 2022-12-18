e7931c8d67fe435455792cc49eb0ff89afa1765b
Shazia Naveeda <114483889+sasapunya@users.noreply.github.com>
2022-12-18 20:44:33 +0700

    Update GameSubScene.java

--- FP_PBO/src/model/GameSubScene.java
+++ FP_PBO/src/model/GameSubScene.java
@@ -13,7 +13,7 @@ import javafx.util.Duration;
 public class GameSubScene extends SubScene {
 
     //private final static String  FONT_PATH = "src/model/resources/kenvector_future.ttf";
-    private final static String BACKGROUND_IMAGE = "model/resources/blue_panel.png";
+    private final static String BACKGROUND_IMAGE = "model/resources/about1.png";
 
     private boolean isHidden;
 
